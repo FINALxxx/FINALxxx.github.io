7091ea1252ca596f29154b8d244e2523a4871462
FINALxxx <3085378853@qq.com>
2026-05-16 15:50:00 +0800

    fix(site): rm site.

--- site/js/mkdocstrings.js
+++
@@ -1,9 +0,0 @@
-for (const el of document.querySelectorAll(
-	".typography .doc code.language-python",
-)) {
-	el.classList.add("codehilite");
-}
-
-for (const el of document.querySelectorAll("article .doc .doc-contents")) {
-	el.innerHTML = el.innerHTML.trim();
-}
