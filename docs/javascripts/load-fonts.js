a092665fc3fe49166050d7a2f786c2ad6a277e96
FINALxxx <3085378853@qq.com>
2026-05-16 13:53:27 +0800

    feat(repo): add js and css.

---
+++ docs/javascripts/load-fonts.js
@@ -0,0 +1,5 @@
+// Load JetBrains Mono font
+var link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
